fa4a2f93f8b8b95226a9fd94cf4660e44a675628
Barak Levav <levavbarak@gmail.com>
2022-03-23 17:44:13 +0200

    upload

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,25 @@
 <template>
   <div class="app-main">
+    <app-header/>
     <router-view></router-view>
+    <app-footer/>
   </div>
 </template>
 
 <script>
-// import { RouterLink, RouterView } from "vue-router";
+import appHeader from './components/app-header.vue'
+import appFooter from './components/app-footer.vue'
+
+
 export default {
   name: "App",
   created() {
     this.$store.dispatch({ type: "loadStays" });
   },
+  components: {
+    appHeader,
+    appFooter
+  }
 };
 </script>
 
