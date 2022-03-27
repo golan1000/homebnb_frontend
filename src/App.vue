7bcb08cb12db3d556811a108f9b9ea146ef39128
Barak Levav <levavbarak@gmail.com>
2022-03-27 10:30:32 +0300

    redo for last commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,29 @@
 <template>
   <div>
-    <app-header/>
+    <app-header />
     <router-view></router-view>
-    <app-footer/>
+    <app-footer />
   </div>
 </template>
 
 <script>
-import appHeader from './components/app-header.vue'
-import appFooter from './components/app-footer.vue'
+import appHeader from "./components/app-header.vue";
+import appFooter from "./components/app-footer.vue";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-
 
 export default {
   name: "App",
+  data() {
+    return {
+    };
+  },
   created() {
     this.$store.dispatch({ type: "loadStays" });
   },
   components: {
     appHeader,
-    appFooter
-  }
+    appFooter,
+  },
 };
 </script>
 
