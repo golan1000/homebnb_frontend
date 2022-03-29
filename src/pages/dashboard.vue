ed84285d79b0b971a59305a4d86bb4c602bbd0a6
Barak Levav <levavbarak@gmail.com>
2022-03-29 18:27:17 +0300

    Barak git after asi finished basic style of backoffice

--- src/pages/dashboard.vue
+++ src/pages/dashboard.vue
@@ -2,35 +2,29 @@
   <div class="dashboard-con">
     <div class="dashboard">
       <div class="dashboard-top-section">
-        <div class="dashboard-summary">
-          <div class="dashboard-summary-stays">
-            <img src="" alt="" class="dashboard-summary-stays-img" />
-            <h4 class="dashboard-summary-stays-title">My Stays</h4>
-          </div>
-          <div class="dashboard-summary-orders">
-            <img src="" alt="" class="dashboard-summary-orders-img" />
-            <h4 class="dashboard-summary-orders-title">My Orders</h4>
-          </div>
-        </div>
         <div class="dashboard-data-con">
           <div class="dashboard-data data-rate">
-            <h4 class="dashboard-data-rate-title">Total Rate</h4>
-            <div class="dashboard-data-rate-average">
-              <img
-                src="../assets/star.svg"
-                alt="star image"
-                class="dashboard-data-rate-img"
-              />
-              <span class="dashboard-data-rate-average-number">0</span>
-              <span class="dashboard-data-rate-average-title">Average</span>
-            </div>
-            <div class="dashboard-data-rate-count">
-              <span class="dashboard-data-rate-count-number">0</span>
-              <span class="dashboard-data-rate-count-title">Reviews</span>
+            <h4 class="dashboard-data-rate-title">Rate</h4>
+            <div class="dashboard-data-rate-content">
+              <div class="dashboard-data-rate-average">
+                <span class="dashboard-data-rate-average-title">Average</span>
+                <div class="dashboard-data-rate-average-content">
+                  <img
+                    src="../assets/star.svg"
+                    alt="star image"
+                    class="dashboard-data-rate-img"
+                  />
+                  <span class="dashboard-data-rate-average-number">0</span>
+                </div>
+              </div>
+              <div class="dashboard-data-rate-count">
+                <span class="dashboard-data-rate-count-title">Reviews</span>
+                <span class="dashboard-data-rate-count-number">0</span>
+              </div>
             </div>
           </div>
           <div class="dashboard-data data-revenues">
-            <h4 class="dashboard-data-revenues-title">Total Revenues</h4>
+            <h4 class="dashboard-data-revenues-title">Revenues</h4>
             <div class="dashboard-data-revenues-period-con">
               <div class="dashboard-data-revenues-period period-month">
                 <span class="dashboard-data-revenues-period-month">Month</span>
@@ -49,31 +43,42 @@
           <div class="dashboard-data data-orders">
             <h4 class="dashboard-data-orders-title">Orders</h4>
             <div class="dashboard-data-orders-status-con">
-              <div class="dashboard-data-orders-status">
-                <span class="dashboard-data-orders-status-title">Total</span>
-                <span class="dashboard-data-orders-status-number status-total"
-                  >0</span
-                >
+              <div class="dashboard-data-orders-status-con-top">
+                <div class="dashboard-data-orders-status">
+                  <span class="dashboard-data-orders-status-title">Total</span>
+                  <span class="dashboard-data-orders-status-number status-total"
+                    >0</span
+                  >
+                </div>
+                <div class="dashboard-data-orders-status">
+                  <span class="dashboard-data-orders-status-title"
+                    >Pending</span
+                  >
+                  <span
+                    class="dashboard-data-orders-status-number status-pending"
+                    >0</span
+                  >
+                </div>
               </div>
-              <div class="dashboard-data-orders-status">
-                <span class="dashboard-data-orders-status-title">Pending</span>
-                <span class="dashboard-data-orders-status-number status-pending"
-                  >0</span
-                >
-              </div>
-              <div class="dashboard-data-orders-status">
-                <span class="dashboard-data-orders-status-title">Approved</span>
-                <span
-                  class="dashboard-data-orders-status-number status-approved"
-                  >0</span
-                >
-              </div>
-              <div class="dashboard-data-orders-status">
-                <span class="dashboard-data-orders-status-title">Declined</span>
-                <span
-                  class="dashboard-data-orders-status-number status-declined"
-                  >0</span
-                >
+              <div class="dashboard-data-orders-status-con-bottom">
+                <div class="dashboard-data-orders-status">
+                  <span class="dashboard-data-orders-status-title"
+                    >Approved</span
+                  >
+                  <span
+                    class="dashboard-data-orders-status-number status-approved"
+                    >0</span
+                  >
+                </div>
+                <div class="dashboard-data-orders-status">
+                  <span class="dashboard-data-orders-status-title"
+                    >Declined</span
+                  >
+                  <span
+                    class="dashboard-data-orders-status-number status-declined"
+                    >0</span
+                  >
+                </div>
               </div>
             </div>
           </div>
@@ -104,16 +109,49 @@
         </div>
       </div>
       <div class="dashboard-bottom-section">
+        <div class="dashboard-summary">
+          <button class="dashboard-summary-stays">
+            <img
+              src="../assets/backoffice-house.svg"
+              alt="stays-img"
+              class="dashboard-summary-stays-img backoffice-img"
+            />
+            <h4 class="dashboard-summary-stays-title">Stays</h4>
+          </button>
+          <button class="dashboard-summary-orders">
+            <img
+              src="../assets/backoffice-list.svg"
+              alt="orders-img"
+              class="dashboard-summary-orders-img backoffice-img"
+            />
+            <h4 class="dashboard-summary-orders-title">Orders</h4>
+          </button>
+        </div>
         <div class="dashboard-details">
-          <table>
-            <th class="dashboard-details-th">Guest Name</th>
-            <th class="dashboard-details-th">Stay Name</th>
-            <th class="dashboard-details-th">Check In - Check Out</th>
-            <th class="dashboard-details-th">Status</th>
-            <th class="dashboard-details-th">Revenue</th>
-            <th class="dashboard-details-th">Actions</th>
-            <tr class="dashboard-details-tr"></tr>
-          </table>
+          <div class="dashboard-details-table stays-table">
+            <table>
+              <th class="dashboard-details-th stays-table-avatar"></th>
+              <th class="dashboard-details-th stays-table-guest">Guest Name</th>
+              <th class="dashboard-details-th stays-table-stay">Stay Name</th>
+              <th class="dashboard-details-th stays-table-check">
+                Check In - Check Out
+              </th>
+              <th class="dashboard-details-th stays-table-status">Status</th>
+              <th class="dashboard-details-th stays-table-revenue">Revenue</th>
+              <th class="dashboard-details-th stays-table-actions">Actions</th>
+              <tr class="dashboard-details-tr stays-table-content"></tr>
+            </table>
+          </div>
+          <div class="dashboard-details-table orders-table">
+            <table>
+              <th class="dashboard-details-th orders-table-avatar"></th>
+              <th class="dashboard-details-th orders-table-name">Name</th>
+              <th class="dashboard-details-th orders-table-price">Price</th>
+              <th class="dashboard-details-th orders-table-address">Address</th>
+              <th class="dashboard-details-th orders-table-actions">Actions</th>
+              <tr class="dashboard-details-tr"></tr>
+            </table>
+          </div>
         </div>
       </div>
     </div>
