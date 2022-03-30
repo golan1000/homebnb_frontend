5e20cbb67b859ec806903d0361927db2aa456f48
TalL <tallevi008@gmail.com>
2022-03-30 12:46:28 +0300

    tal: orders store

--- src/store/modules/order-module.js
+++ src/store/modules/order-module.js
@@ -1,4 +1,6 @@
-import { orderService } from '../../services/order-service'
+// import { orderService } from '../../services/order-service'
+import { orderService } from '../../services/order.service.async.js';
+
 export default {
   state: {
     orders: [],
@@ -6,30 +8,30 @@ export default {
   getters: {},
   mutations: {
     submitOrder(state, { order }) {
-      console.log('mutate ---- submitOrder=', order)
+      console.log('mutate ---- submitOrder=', order);
 
-      this.orders.push(order)
+      this.orders.push(order);
     },
   },
   actions: {
     // Tal converted to async
     async submitOrder(context, { order }) {
-      console.log('action ---- submitOrder=', order)
+      console.log('action ---- submitOrder=', order);
       try {
-        let orderId = await orderService.save(order)
-        context.showMsg('Your order is pending...thank you')
-        context.commit({ type: 'submitOrder', order })
+        let orderId = await orderService.save(order);
+        context.showMsg('Your order is pending...thank you');
+        context.commit({ type: 'submitOrder', order });
       } catch (err) {
         context.showMsg(
-          'We had an error while submitting your order, try again later',
-        )
+          'We had an error while submitting your order, try again later'
+        );
       }
     },
     showMsg(msg) {
-      console.log(msg)
+      console.log(msg);
     },
   },
-}
+};
 
 // Golan original
 // submitOrder(context, { order }) {
