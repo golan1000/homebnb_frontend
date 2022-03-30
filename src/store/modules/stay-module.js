e48aab00604594667bde7acbf0e5e3ad0c6b48bb
golan1000 <golanabc10100@gmail.com>
2022-03-30 22:15:54 +0300

    changed app vue created to async

--- src/store/modules/stay-module.js
+++ src/store/modules/stay-module.js
@@ -67,6 +67,7 @@ export default {
       try {
         const stays = await stayService.query(state.filterBy)
         commit({ type: 'setStays', stays })
+        return stays
       } catch (err) {
         console.log('err in stay-module in loadToys:', err)
       }
